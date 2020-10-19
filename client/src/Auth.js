fff819b2da6cd664e6a395dce5eec9e099f77788
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-19 19:59:45 +1100

    Used reducer to make asynchronous axios request from mongodb.

--- client/src/Auth.js
+++ client/src/Auth.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
-import { login } from './Auth.redux'
+import { login, getUserData } from './Auth.redux'
 
 class Auth extends React.Component {
+  componentDidMount() {
+    this.props.getUserData()
+  }
   render() {
     return (
       <div>
+        <h2>My name is {this.props.user}, and my age is {this.props.age}</h2>
         {this.props.isAuth ? <Redirect to='/dashboard'/> : null}
         <h2>You need to login to proceed</h2>
         <button onClick={this.props.login}>Login</button>
@@ -15,9 +19,11 @@ class Auth extends React.Component {
   }
 }
 const mapStatetoProps = (state) => {
-  return {isAuth: state.auth.isAuth}
+  return {
+    isAuth: state.auth.isAuth, user: state.auth.user, age: state.auth.age
+  }
 }
-const actionCreators = {login}
+const actionCreators = {login, getUserData}
 Auth = connect(mapStatetoProps, actionCreators) (Auth)
 
 export default Auth
--- client/src/Auth.redux.js
+++ client/src/Auth.redux.js
@@ -1,19 +1,44 @@
+import axios from "axios"
+
 const LOGIN = 'LOGIN'
 const LOGOUT = 'LOGOUT'
+const USER_DATA = 'USER_DATA'
+const initState = {
+  isAuth: false,
+  user: 'Ben',
+  age: 36
+}
 
 // reducer
-export function auth(state={isAuth: false, user:'uzi'}, action) {
+export function auth(state=initState, action) {
   switch(action.type) {
     case LOGIN:
       return {...state, isAuth: true}
     case LOGOUT:
       return {...state, isAuth: false}
+    case USER_DATA:
+      return {...state, user: action.payload[0].user, age: action.payload[0].age}
     default:
       return state
   }
 }
 
 // action creator
+// Asynchronous axios request
+export function getUserData() {
+  // use disptach to notify that state has been changed 
+  return dispatch => {
+    axios.get('/data')
+      .then(res => {
+        if (res.status === 200) {
+          dispatch(userData(res.data))
+        }
+      })
+  }
+}
+export function userData(data) {
+  return {type: USER_DATA, payload: data}
+}
 export function login() {
   return {type: LOGIN}
 }
