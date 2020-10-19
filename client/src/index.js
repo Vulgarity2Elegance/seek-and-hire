1726d386f082375dc14370d3a38a4638eb490e9d
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-19 15:31:45 +1100

    Created Routes for login and dashboard routes & Used redux to control their states respectively.

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,25 @@ import ReactDom from 'react-dom'
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from "react-redux"
-import App from "./App"
-import {counter} from './index.redux'
+import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom'
 
-const store = createStore(counter, compose(
+import reducers from "./reducer"
+import Auth from "./Auth"
+import Dashboard from "./Dashboard"
+
+const store = createStore(reducers, compose(
   applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f=>f
 ) )
 
 ReactDom.render(
   (<Provider store={store}>
-    <App/>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/login' component={Auth} />
+        <Route path='/dashboard' component={Dashboard} />
+        <Redirect to='/dashboard' />
+      </Switch>
+    </BrowserRouter>
   </Provider>), 
   document.getElementById('root')
 )
