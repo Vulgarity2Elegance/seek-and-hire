cf10bf343e38e37ba7bc474e07f2739ac104ecd6
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-21 10:20:23 +1100

    Created the User model.

--- models/model.js
+++ models/model.js
@@ -4,3 +4,29 @@ const mongoose = require('mongoose')
 const DB_URL = 'mongodb://localhost:27017/seekAndHire'
 mongoose.connect(DB_URL)
 mongoose.connection.on('connected', () => console.log('Connected Successfully'))
+
+const models = {
+  user: {
+    'username': {type: String, require: true},
+    'password': {type: String, require: true},
+    'type': {type: String, require: true},
+    'avatar': {type: String},
+    'bio': {type: String},
+    'title': {type: String},
+    'company': {type: String},
+    'salary': {type: String}
+  },
+  chat: {
+
+  }
+}
+
+for (let model in models) {
+  mongoose.model(model, new mongoose.Schema(models[model]))
+}
+
+module.exports = {
+  getModel: function(name){
+    return mongoose.model(name)
+  }
+}
