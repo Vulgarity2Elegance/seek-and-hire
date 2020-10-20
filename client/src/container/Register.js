8d6e9cef2f5547dda6f680477f3d8e2a0e13c520
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-21 10:43:48 +1100

    Used connect() connects Register component to user.redux store.

--- client/src/container/Register.js
+++ client/src/container/Register.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Logo from '../components/Logo/Logo'
 import { Button, InputItem, List, Radio, WhiteSpace, WingBlank } from 'antd-mobile'
+import { connect } from 'react-redux'
+import { register } from '../redux/user.redux'
+import '../index.css'
 
 class Register extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      user: '',
-      pwd: '',
-      valid:'',
+      username: '',
+      password: '',
+      verification:'',
       type: 'seeker'
     }
     this.handleRegister = this.handleRegister.bind(this)
@@ -19,6 +22,7 @@ class Register extends React.Component {
     })
   }
   handleRegister() {
+    this.props.register(this.state)
     console.log(this.state)
   }
   render() {
@@ -29,9 +33,10 @@ class Register extends React.Component {
         <h1>Register</h1>
         <WingBlank>
           <List>
-            <InputItem onChange={v => this.handleChange('user', v)}>Username</InputItem>
-            <InputItem type='password' onChange={v => this.handleChange('pwd', v)}>Password</InputItem>
-            <InputItem type='password' onChange={v => this.handleChange('valid', v)}>Confirm</InputItem>
+            {this.props.msg ? <p className='err-msg'>{this.props.msg}</p> : null}
+            <InputItem onChange={v => this.handleChange('username', v)}>Username</InputItem>
+            <InputItem type='password' onChange={v => this.handleChange('password', v)}>Password</InputItem>
+            <InputItem type='password' onChange={v => this.handleChange('verification', v)}>Confirm</InputItem>
             <RadioItem checked={this.state.type === 'seeker'} onChange={() => this.handleChange('type', 'seeker')}>Job Seeker</RadioItem>
             <RadioItem checked={this.state.type === 'hirer'} onChange={() => this.handleChange('type', 'hirer')}>Job Hirer</RadioItem>
           </List>
@@ -42,5 +47,14 @@ class Register extends React.Component {
     )
   }
 }
+
+const mapStatetoProps = (state) => {
+  return {
+    user: state.user,
+    msg: state.user.msg
+  }
+}
+const actionCreators = {register}
+Register = connect(mapStatetoProps, actionCreators) (Register)
 
 export default Register
