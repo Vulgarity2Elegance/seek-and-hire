f0db4cdb67955ca6c8c9c1db73660d1ee622beb7
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-20 18:01:45 +1100

    Updated routes and removed unused imported components.

--- client/src/index.js
+++ client/src/index.js
@@ -3,24 +3,32 @@ import ReactDom from 'react-dom'
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from "react-redux"
-import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom'
+import {BrowserRouter, Route} from 'react-router-dom'
 
+import Login from "./container/Login"
+import Register from "./container/Register"
 import reducers from "./reducer"
-import Auth from "./Auth"
-import Dashboard from "./Dashboard"
+import "./config"
+import 'antd-mobile/dist/antd-mobile.css'
+import AuthRoute from './components/AuthRoute/AuthRoute'
 
 const store = createStore(reducers, compose(
   applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f=>f
 ) )
 
+function Boss() {
+  return <h2>Boss</h2>
+}
+
 ReactDom.render(
   (<Provider store={store}>
     <BrowserRouter>
-      <Switch>
-        <Route path='/login' component={Auth} />
-        <Route path='/dashboard' component={Dashboard} />
-        <Redirect to='/dashboard' />
-      </Switch>
+      <div>
+        <AuthRoute/>
+        <Route path='/boss' component={Boss} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+      </div>
     </BrowserRouter>
   </Provider>), 
   document.getElementById('root')
