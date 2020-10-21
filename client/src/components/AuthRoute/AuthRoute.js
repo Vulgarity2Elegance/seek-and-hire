355197dcb50b05ecf594fc378c46f4534bde5203
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-21 23:40:21 +1100

    Used 'connect()' to display userinfo once the user is loggedin & registered.

--- client/src/components/AuthRoute/AuthRoute.js
+++ client/src/components/AuthRoute/AuthRoute.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import axios from 'axios'
 import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { loadData } from '../../redux/user.redux'
 
 class AuthRoute extends React.Component {
   componentDidMount() {
@@ -9,26 +11,25 @@ class AuthRoute extends React.Component {
     if (publicList.indexOf(pathname) > -1) {
       return null
     }
-    console.log(this.props)
     // get users' info including current url address, users' type, information completion 
     axios.get('/user/info')
       .then(res => {
         if (res.status === 200) {
           if (res.data.code === 0) {
-
+            this.props.loadData(res.data.data)
           } else {
-            console.log(this.props.history)
             this.props.history.push('/login')
           }
-          console.log(res.data)
         }
       })
   }
   render() {
-    return(
-      <p>redirect</p>
-    )
+    return null
   }
 }
+
+const mapStatetoProps = (state) => state.user
+const actionCreators = {loadData}
+AuthRoute = connect(mapStatetoProps, actionCreators) (AuthRoute)
 
 export default withRouter(AuthRoute)
