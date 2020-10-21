ec7794d6d69012622462e7d55a71d09c828ec6ec
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-22 09:47:56 +1100

    SeekerInfo.js replicates HirerInfo.js.

---
+++ client/src/container/SeekerInfo.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Button, InputItem, NavBar, TextareaItem, WhiteSpace } from 'antd-mobile'
+import AvatarSelector from '../components/AvatarSelector/AvatarSelector'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+import { update } from '../redux/user.redux'
+
+class SeekerInfo extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: '',
+      company: '',
+      salary: '',
+      bio: ''
+    }
+  }
+  onChange(key, val) {
+    this.setState({
+      [key]: val
+    })
+  }
+  render() {
+    const path = this.props.location.pathname
+    const redirect = this.props.redirectTo
+    return(
+      <div>
+        {redirect && redirect !== path 
+          ? <Redirect to={this.props.redirectTo}/> 
+          : null}
+        <NavBar mode='dark'>SeekerInfo Dashboard</NavBar>
+        <AvatarSelector selectAvatar={(imgName) => {
+          this.setState({
+            avatar: imgName
+          })
+        }}/>
+        <WhiteSpace />
+        <InputItem onChange={(v) => this.onChange('title', v)}>Title</InputItem>
+        <InputItem onChange={(v) => this.onChange('company', v)}>Company</InputItem>
+        <InputItem onChange={(v) => this.onChange('salary', v)}>Salary</InputItem>
+        <TextareaItem onChange={(v) => this.onChange('bio', v)} rows={3} autoHeight title='Bio' />
+        <WhiteSpace />
+        <Button 
+          onClick={() => {
+            this.props.update(this.state)
+          }}
+          type='primary'>Save</Button>
+      </div>
+    )
+  }
+}
+
+const mapStatetoProps = (state) => state.user
+const actionCreators = {update}
+SeekerInfo = connect(mapStatetoProps, actionCreators) (SeekerInfo)
+
+export default SeekerInfo
