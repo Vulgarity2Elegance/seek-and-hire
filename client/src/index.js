3270c4ea7260f646d372309082f4025f80722eec
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-22 08:37:37 +1100

    Added the 'hirerinfo' route.

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDom from 'react-dom'
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from "react-redux"
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import Login from "./container/Login"
 import Register from "./container/Register"
@@ -11,23 +11,22 @@ import reducers from "./reducer"
 import "./config"
 import 'antd-mobile/dist/antd-mobile.css'
 import AuthRoute from './components/AuthRoute/AuthRoute'
+import HirerInfo from './container/HirerInfo'
 
 const store = createStore(reducers, compose(
   applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f=>f
 ) )
-
-function Boss() {
-  return <h2>Boss</h2>
-}
 
 ReactDom.render(
   (<Provider store={store}>
     <BrowserRouter>
       <div>
         <AuthRoute/>
-        <Route path='/boss' component={Boss} />
-        <Route path='/login' component={Login} />
-        <Route path='/register' component={Register} />
+        <Switch>
+          <Route path='/hirerinfo' component={HirerInfo} />
+          <Route path='/login' component={Login} />
+          <Route path='/register' component={Register} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>), 
