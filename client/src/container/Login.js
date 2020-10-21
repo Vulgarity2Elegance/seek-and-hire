43c4c31c529f7edbd9d49924930128649a5ea3af
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-21 23:42:08 +1100

    Replicated Register.js for Login.js.

--- client/src/container/Login.js
+++ client/src/container/Login.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import Logo from "../components/Logo/Logo"
 import {List, InputItem, WhiteSpace, Button, WingBlank} from 'antd-mobile'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+import { login } from '../redux/user.redux'
 
 class Login extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      username: '',
+      password: ''
+    }
     this.register = this.register.bind(this)
+    this.handleLogin = this.handleLogin.bind(this)
+  }
+  handleChange(key, val) {
+    this.setState({
+      [key]: val
+    })
+  }
+  handleLogin() {
+    this.props.login(this.state)
   }
   register(){
     console.log(this.props)
@@ -14,15 +30,16 @@ class Login extends React.Component {
   render() {
     return (
       <div>
+        {this.props.redirectTo ? <Redirect to={this.props.redirectTo}/> : null}
         <Logo/>
-        <h1>Login</h1>
         <WingBlank>
           <List>
-            <InputItem>User</InputItem>
-            <InputItem>Password</InputItem>
+            {this.props.msg ? <p className='err-msg'>{this.props.msg}</p> : null}
+            <InputItem onChange={v => this.handleChange('username', v)}>Username</InputItem>
+            <InputItem type='password' onChange={v => this.handleChange('password', v)}>Password</InputItem>
           </List>
           <WhiteSpace/>
-          <Button type='primary'>Login</Button>
+          <Button onClick={this.handleLogin} type='primary'>Login</Button>
           <WhiteSpace/>
           <Button onClick={this.register} type='primary'>Register</Button>
         </WingBlank>
@@ -30,5 +47,9 @@ class Login extends React.Component {
     )
   }
 }
+
+const mapStatetoProps = (state) => state.user
+const actionCreators = {login}
+Login = connect(mapStatetoProps, actionCreators) (Login)
 
 export default Login
