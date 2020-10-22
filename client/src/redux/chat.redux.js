4e2a1467b01ee7e2130778319d6cd2d612458f4e
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-23 09:55:35 +1100

    Created action.type 'userList' according to thier type.

---
+++ client/src/redux/chat.redux.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+const USER_LIST = 'USER_LIST'
+
+const initState = {
+  userList: []
+}
+
+// Reducer
+export function chat(state=initState, action) {
+  switch (action.type) {
+    case USER_LIST:
+      return {...state, userList: action.payload}
+    default: 
+      return state
+  }
+}
+
+
+
+// Action Creators
+function userList(data) {
+  return {type: USER_LIST, payload: data}
+}
+
+export function getUserList(type) {
+  return dispatch => {
+    axios.get('/user/list?type=' + type)
+      .then(res => {
+        if (res.data.code === 0) {
+          dispatch(userList(res.data.data))
+        }
+      })
+  }
+}
