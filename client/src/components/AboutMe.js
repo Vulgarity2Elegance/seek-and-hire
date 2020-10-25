b0ce5e960c45295e42a5cbd79df74c093abbf1d8
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-25 15:39:17 +1100

    Rendered users' info by using Card and Result from ant design. Erase userId from cookies when logout.

--- client/src/components/AboutMe.js
+++ client/src/components/AboutMe.js
@@ -1,51 +1,59 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Button, List, Result, WhiteSpace } from 'antd-mobile'
+import { Button, List, Modal, Result, WhiteSpace, WingBlank } from 'antd-mobile'
 import ListItem, { Brief } from 'antd-mobile/lib/list/ListItem'
+import cookies from 'browser-cookies'
+import { logoutSubmit } from '../redux/user.redux'
+import { Redirect } from 'react-router-dom'
 
 class AboutMe extends React.Component {
+  constructor(props) {
+    super(props)
+    this.logout = this.logout.bind(this)
+  }
+  logout() {
+    const alert = Modal.alert
+    alert('Logout', 'Are you sure???', [
+      { text: 'Cancel', onPress: () => console.log('cancel'), style: 'default' },
+      { text: 'OK', onPress: () => {
+        cookies.erase('userId')
+        this.props.logoutSubmit()
+      }},
+    ]);
+  }
   render() {
-    return(
-      this.props.username
-      ? 
-        <div>
-          <Result
-            img={ <img src={require(`./AvatarSelector/img/${this.props.avatar}.png`)} alt='avatar'/> }
-            title={this.props.username}
-          />
-          <WhiteSpace size='xl' />
-          <List renderHeader={() => 'Title'}>
-            <ListItem multipleLine> {this.props.title} </ListItem>
-          </List>
-          <List renderHeader={() => 'Company'}>
-            <ListItem multipleLine> {this.props.company} </ListItem>
-          </List>
-          <List renderHeader={() => 'Salary'}>
-            <ListItem multipleLine> {this.props.salary} </ListItem>
-          </List>
-          <List renderHeader={() => 'Description'}>
-            <ListItem>
-              {this.props.bio.split('\n').map(v => <Brief key={v}> {v} </Brief>)}
-            </ListItem>
-          </List>
-          <WhiteSpace size='xl'/>
-          <Button 
-            type='warning'
-            onClick={() => {
-              console.log('mom')
-            }}
-          >
-            Logout
-          </Button>
-        </div>
-      : 
-        null
-    )
+    const props = this.props
+    return props.username ? (
+    <div>
+      <Result
+      img={ <img src={require(`./AvatarSelector/img/${props.avatar}.png`)} alt='avatar'/> }
+      title={props.username}
+      /> 
+      <WhiteSpace size='xl' />
+      <List renderHeader={() => 'Title'}>
+        <ListItem multipleLine> {props.title} </ListItem>
+      </List>
+      <List renderHeader={() => 'Company'}>
+        <ListItem multipleLine> {props.company} </ListItem>
+      </List>
+      <List renderHeader={() => 'Salary'}>
+        <ListItem multipleLine> {props.salary} </ListItem>
+      </List>
+      <List renderHeader={() => 'Description'}>
+        <ListItem>
+          {props.bio.split('\n').map(v => <Brief key={v}> {v} </Brief>)}
+        </ListItem>
+      </List>
+      <WhiteSpace size='xl'/>
+      <WingBlank>
+        <Button type='warning' size='large' onClick={this.logout()}>Logout</Button>
+      </WingBlank>
+    </div>) : <Redirect to={props.redirectTo} />
   }
 }
 
 const mapStatetoProps = (state) => state.user
-const actionCreators = {}
+const actionCreators = {logoutSubmit}
 AboutMe = connect(mapStatetoProps, actionCreators) (AboutMe)
 
 export default AboutMe
