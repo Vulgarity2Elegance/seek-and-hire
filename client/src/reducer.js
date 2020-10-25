14acc17b455eecb2d2d2ca757f23aac6bb72ff05
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-25 16:32:11 +1100

    Added message reducer.

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -2,5 +2,6 @@
 import {combineReducers} from "redux"
 import {user} from "./redux/user.redux"
 import {chat} from "./redux/chat.redux"
+import {message} from "./redux/message.redux"
 
-export default combineReducers({user, chat})
+export default combineReducers({user, chat, message})
