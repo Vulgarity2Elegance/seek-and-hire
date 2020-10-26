ce06c2680d4c489aab94a5aa510ac9fcd6f29f01
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-26 20:02:16 +1100

    Show unread message on the msgList.

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { List } from 'antd-mobile'
+import { Badge, List } from 'antd-mobile'
 import { Brief } from 'antd-mobile/lib/list/ListItem'
 
 class Message extends React.Component {
@@ -21,18 +21,34 @@ class Message extends React.Component {
 
     // Get values of msgGroup 
     const chatList = Object.values(msgGroup)
+    console.log(chatList)
+    
+    chatList.sort((a, b) => {
+      // sort chatList according to creat_time
+      const a_last = this.getLast(a).create_time
+      console.log(a_last)
+      const b_last = this.getLast(b).create_time
+      console.log(b_last)
+      return a_last - b_last
+    })
 
     return (
       <div>
         {chatList.map(v => {
           const lastItem = this.getLast(v)
           const targetId = v[0].from === userid ? v[0].to : v[0].from
+          // Show unread message on the msgList
+          const unreadNum = v.filter(v => !v.read && v.to === userid).length
 
           return (
-            <List key={lastItem.create_time}>
+            <List key={lastItem.chatId}>
               <List.Item 
-                key={lastItem.create_time}
+                extra={<Badge text={unreadNum}/>}
                 thumb={require(`../components/AvatarSelector/img/${userinfo[targetId].avatar}.png`)}
+                arrow='horizontal'
+                onClick={() => {
+                  this.props.history.push(`/chat/${targetId}`)
+                }}
               >
                 {userinfo[targetId].name}
                 <Brief> {lastItem.content} </Brief>
