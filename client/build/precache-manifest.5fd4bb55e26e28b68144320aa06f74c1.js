227f13ff2aad4b8c5f3d6fd1b0f9497e32453a12
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-27 16:51:51 +1100

    deploying

---
+++ client/build/precache-manifest.5fd4bb55e26e28b68144320aa06f74c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5506feeb8c8bd565acd2462dd9348de7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31bf854f3b57f26dfe82",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "52c896d172877dcaf193",
+    "url": "/static/css/main.92b57b9b.chunk.css"
+  },
+  {
+    "revision": "31bf854f3b57f26dfe82",
+    "url": "/static/js/2.3b39a45c.chunk.js"
+  },
+  {
+    "revision": "7da3f168e2e145ab8f34661363b07d49",
+    "url": "/static/js/2.3b39a45c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52c896d172877dcaf193",
+    "url": "/static/js/main.b3f2d5be.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "11bfc57c585e547b238c400fc73cfdc2",
+    "url": "/static/media/Logo.11bfc57c.png"
+  }
+]);
