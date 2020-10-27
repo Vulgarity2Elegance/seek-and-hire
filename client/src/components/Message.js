46a12eec20236ffd4eff22db4431494f55db4729
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-27 14:47:50 +1100

    Removed sort function. Bugs fixed.

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Badge, List } from 'antd-mobile'
-import { Brief } from 'antd-mobile/lib/list/ListItem'
+import ListItem, { Brief } from 'antd-mobile/lib/list/ListItem'
 
 class Message extends React.Component {
   getLast(arr) {
@@ -12,37 +12,29 @@ class Message extends React.Component {
     const userid = this.props.user._id
     // getMsgList() 
     const userinfo = this.props.message.users
-    // Group by Corrspodence filter by chatID
+
+    // Group by Corrspodence with users filter by chatID
     const msgGroup = {}
     this.props.message.chatMsg.forEach(v => {
       msgGroup[v.chatId] = msgGroup[v.chatId] || []
       msgGroup[v.chatId].push(v)
     })
-
+   
     // Get values of msgGroup 
     const chatList = Object.values(msgGroup)
-    console.log(chatList)
-    
-    chatList.sort((a, b) => {
-      // sort chatList according to creat_time
-      const a_last = this.getLast(a).create_time
-      console.log(a_last)
-      const b_last = this.getLast(b).create_time
-      console.log(b_last)
-      return a_last - b_last
-    })
 
     return (
       <div>
         {chatList.map(v => {
+          // The data of the last message
           const lastItem = this.getLast(v)
           const targetId = v[0].from === userid ? v[0].to : v[0].from
           // Show unread message on the msgList
           const unreadNum = v.filter(v => !v.read && v.to === userid).length
-
+          if (!userinfo[targetId]) {return null}
           return (
             <List key={lastItem.chatId}>
-              <List.Item 
+              <ListItem 
                 extra={<Badge text={unreadNum}/>}
                 thumb={require(`../components/AvatarSelector/img/${userinfo[targetId].avatar}.png`)}
                 arrow='horizontal'
@@ -52,7 +44,7 @@ class Message extends React.Component {
               >
                 {userinfo[targetId].name}
                 <Brief> {lastItem.content} </Brief>
-              </List.Item>
+              </ListItem>
             </List>
           )
         })}
@@ -62,7 +54,6 @@ class Message extends React.Component {
 }
 
 const mapStatetoProps = (state) => state
-// const actionCreators = {getMsgList, sendMsg, receiveMsg}
 Message = connect(mapStatetoProps) (Message)
 
 export default Message
